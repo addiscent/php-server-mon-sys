53c722ef387667890f6702d3a8fa9f9bf21a79c9
Rex Addiscentis <raddiscentis@addiscent.com>
2015-07-04 11:56:43 -0700

    Correct sub-dir path in comment

--- dl-untar-psm.sh
+++ dl-untar-psm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# download phpservermon-3.1.1, untar it.  Files resting place is www/phpservermon
+# download phpservermon-3.1.1, untar it.  Files resting place is src/public/phpservermon
 curl -L -o phpservermon-3.1.1.tar.gz https://sourceforge.net/projects/phpservermon/files/phpservermon/PHP%20Server%20Monitor%20v3.1.1/phpservermon-3.1.1.tar.gz/download \
 && mkdir src/public/ \
 && tar -C src/public/ -zxvf phpservermon-3.1.1.tar.gz \
